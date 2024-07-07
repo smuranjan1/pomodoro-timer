19359e60ec2073c44d59f2d35456d360936b55e8
smuranjan1 <smyan.muranjan@gmail.com>
2024-07-07 13:18:24 -0500

    Added work and break time.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [workTime, setWorkTime] = useState(1500);
+  const [breakTime, setBreakTime] = useState(300);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  function displayTime(time) {
+    const minutes = String(time / 60);
+    const seconds = String(time % 60);
+    return `${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}`;
+  }
+
+  const addWorkTime = () => workTime < 3600 ? setWorkTime(workTime + 60) : alert("Max Work Time");
+  const removeWorkTime = () => workTime > 0 ? setWorkTime(workTime - 60) : alert("Min Work Time");
+  const addBreakTime = () => breakTime < 3600 ? setBreakTime(breakTime + 60) : alert("Max Break Time");
+  const removeBreakTime = () => breakTime > 0 ? setBreakTime(breakTime - 60) : alert("Min Break Time");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Work Time: {displayTime(workTime)}</p>
+      <p>Break Time: {displayTime(breakTime)}</p>
+      <button onClick={addWorkTime}>Increase Work Time</button>
+      <button onClick={removeWorkTime}>Decrease Work Time</button><br />
+      <button onClick={addBreakTime}>Increase Break Time</button>
+      <button onClick={removeBreakTime}>Decrease Break Time</button><br />
+      <button>Start</button>
+      <button>Reset</button>
+    </>
   );
 }
 
 export default App;
+
